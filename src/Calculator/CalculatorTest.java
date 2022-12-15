211b9400c009c91ddc41f0409ab08ae9bae5d7ce
William Attard <attardw@umich.edu>
2022-12-15 16:29:03 -0500

    Committed test2

--- src/Calculator/CalculatorTest.java
+++ src/Calculator/CalculatorTest.java
@@ -10,6 +10,7 @@ private Calculator temp1;
 	
 	@Test
 	void test() {
+		//test1
 	int a = 4;
 	int b = 5;
 		Calculator temp1 = new Calculator();
@@ -18,6 +19,16 @@ private Calculator temp1;
         assertEquals(9, finalTemp, "error in add()");
         assertNotEquals(-2147483647, finalTemp, "error in add()");
         assertNotEquals(2147483647, finalTemp,"error in add()");
+        //test 2
+         a = 4;
+    	 b = 5;
+    		temp1 = new Calculator();
+            finalTemp = temp1.add(a, b);
+    		
+            assertEquals(9, finalTemp, "error in add()");
+            assertNotEquals(-2147483647, finalTemp, "error in add()");
+            assertNotEquals(2147483647, finalTemp,"error in add()");
+            
 	}
 
 }
